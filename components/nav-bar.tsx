babde09fd32d9d64828a6e76e2f7211a4f3a8598
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-12-08 12:39:29 +0530

    Change the navigation links in nav bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,20 +1,18 @@
+import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import {
+	Button,
 	Disclosure,
 	DisclosureButton,
 	DisclosurePanel,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-	{ name: "Dashboard", href: "#", current: true },
-	{ name: "Team", href: "#", current: false },
-	{ name: "Projects", href: "#", current: false },
-	{ name: "Calendar", href: "#", current: false },
+	{ name: "Home", href: "/", current: false },
+	{ name: "Classes", href: "/classes", current: false },
+	{ name: "Teachers", href: "/teachers", current: false },
 ];
 
 export default function NavBar() {
@@ -39,16 +37,14 @@ export default function NavBar() {
 					</div>
 					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 						<div className="flex shrink-0 items-center">
-							<img
-								alt="Your Company"
-								src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-								className="h-8 w-auto"
-							/>
+							<span className="font-mono font-bold text-primary transition-colors hover:text-green-500">
+								VISHWA
+							</span>
 						</div>
 						<div className="hidden sm:ml-6 sm:block">
 							<div className="flex space-x-4">
 								{navigation.map((item) => (
-									<a
+									<Link
 										key={item.name}
 										href={item.href}
 										aria-current={item.current ? "page" : undefined}
@@ -60,64 +56,35 @@ export default function NavBar() {
 										)}
 									>
 										{item.name}
-									</a>
+									</Link>
 								))}
 							</div>
 						</div>
 					</div>
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-						<button
-							type="button"
-							className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-						>
-							<span className="absolute -inset-1.5" />
-							<span className="sr-only">View notifications</span>
-							<BellIcon aria-hidden="true" className="size-6" />
-						</button>
-
-						{/* Profile dropdown */}
-						<Menu as="div" className="relative ml-3">
-							<div>
-								<MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-									<span className="absolute -inset-1.5" />
-									<span className="sr-only">Open user menu</span>
-									<img
-										alt=""
-										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										className="size-8 rounded-full"
-									/>
-								</MenuButton>
-							</div>
-							<MenuItems
-								transition
-								className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+						<SignedIn>
+							<button
+								type="button"
+								className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 							>
-								<MenuItem>
-									<a
-										href="#"
-										className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-									>
-										Your Profile
-									</a>
-								</MenuItem>
-								<MenuItem>
-									<a
-										href="#"
-										className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-									>
-										Settings
-									</a>
-								</MenuItem>
-								<MenuItem>
-									<a
-										href="#"
-										className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-									>
-										Sign out
-									</a>
-								</MenuItem>
-							</MenuItems>
-						</Menu>
+								<span className="absolute -inset-1.5" />
+								<span className="sr-only">View notifications</span>
+								<BellIcon aria-hidden="true" className="size-6" />
+							</button>
+						</SignedIn>
+
+						<div className="relative ml-3 flex justify-center">
+							<SignedOut>
+								<SignInButton>
+									<Button className="rounded bg-primary px-4 py-2 text-sm text-white transition-colors data-[active]:bg-green-500 data-[hover]:bg-green-600">
+										Sign In
+									</Button>
+								</SignInButton>
+							</SignedOut>
+							<SignedIn>
+								<UserButton />
+							</SignedIn>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -127,7 +94,7 @@ export default function NavBar() {
 					{navigation.map((item) => (
 						<DisclosureButton
 							key={item.name}
-							as="a"
+							as={Link}
 							href={item.href}
 							aria-current={item.current ? "page" : undefined}
 							className={cn(
