ff25674566a75ed5736f35c6f8c26319016efd05
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-12-08 13:03:50 +0530

    Change the colors and main paddings

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
 	return (
-		<div className="flex justify-center pt-10">
+		<div className="flex justify-center py-10">
 			<SignIn />
 		</div>
 	);
