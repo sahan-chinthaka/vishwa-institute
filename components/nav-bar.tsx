5c343acb94e724d0902948e54457191c67bb37af
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-12-08 15:24:55 +0530

    Expand the nav bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -17,6 +17,7 @@ const navigation = [
 	{ name: "Teachers", href: "/teachers" },
 	{ name: "News", href: "/news" },
 	{ name: "Contact Us", href: "/contact-us" },
+	{ name: "VLE", href: "/vle" },
 ];
 
 export default function NavBar() {
@@ -24,9 +25,9 @@ export default function NavBar() {
 
 	return (
 		<Disclosure as="nav" className="bg-gray-800">
-			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+			<div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
 				<div className="relative flex h-16 items-center justify-between">
-					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+					<div className="absolute inset-y-0 left-0 flex items-center md:hidden">
 						{/* Mobile menu button*/}
 						<DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 							<span className="absolute -inset-0.5" />
@@ -41,7 +42,7 @@ export default function NavBar() {
 							/>
 						</DisclosureButton>
 					</div>
-					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+					<div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
 						<div className="flex shrink-0 items-center">
 							<Link
 								href="/"
@@ -50,7 +51,7 @@ export default function NavBar() {
 								VISHWA
 							</Link>
 						</div>
-						<div className="hidden sm:ml-6 sm:block">
+						<div className="hidden md:ml-6 md:block">
 							<div className="flex space-x-4">
 								{navigation.map((item) => (
 									<Link
@@ -72,7 +73,7 @@ export default function NavBar() {
 							</div>
 						</div>
 					</div>
-					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+					<div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
 						<SignedIn>
 							<button
 								type="button"
@@ -100,7 +101,7 @@ export default function NavBar() {
 				</div>
 			</div>
 
-			<DisclosurePanel className="sm:hidden">
+			<DisclosurePanel className="md:hidden">
 				<div className="space-y-1 px-2 pb-3 pt-2">
 					{navigation.map((item) => (
 						<DisclosureButton
