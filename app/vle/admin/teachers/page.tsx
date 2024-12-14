a631d92fc585fbf17480fd32f02ea6bfc21f9a22
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-12-14 20:24:18 +0530

    Teachers view created in /vle/admin/teachers

--- app/vle/admin/teachers/page.tsx
+++ app/vle/admin/teachers/page.tsx
@@ -1,15 +1,57 @@
 import { Button } from "@/components/ui/button";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { TeacherForm } from "@/lib/forms";
+import connectMongo from "@/lib/mongo";
+import Teacher from "@/models/teacher";
 import Link from "next/link";
+import { z } from "zod";
 
-function AdminTeacherManagement() {
+async function AdminTeacherManagement() {
+	await connectMongo();
+
+	let teachers = await Teacher.find<z.infer<typeof TeacherForm>>();
+
 	return (
-		<div>
+		<div className="space-y-5 p-5">
 			<div>
 				<Link href="./teachers/new-teacher">
 					<Button>New Teacher</Button>
 				</Link>
 			</div>
-			Teacher
+			<p>Teachers</p>
+			<div>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>#</TableHead>
+							<TableHead>Teacher Name</TableHead>
+							<TableHead>Action</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{teachers.map((teacher, index) => (
+							<TableRow key={index}>
+								<TableCell>{index + 1}</TableCell>
+								<TableCell>
+									{teacher.firstName} {teacher.lastName}
+								</TableCell>
+								<TableCell>
+									<Link href={`./teachers/${teacher.clerkId}`}>
+										<Button variant="outline">Edit</Button>
+									</Link>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
 		</div>
 	);
 }
